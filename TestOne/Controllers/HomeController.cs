5f37483217888e62d693c2e6aba3b117a14e8fe8
Rafal Pawlowski (PGS Software) <rpawlowski@pgs-soft.com>
2018-08-22 15:14:27 +0200

    Add project files.

---
+++ TestOne/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestOne.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(BuzzViewModel model)
+        {
+            return View();
+        }
+        public ActionResult Add()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ TestOne/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TestOne.MvcApplication" Language="C#" %>
